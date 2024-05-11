5e6393ba26d6bf6a51a4a72ff668e011c12a5af4
Patryk Okpara <patrykokpara@hotmail.com>
2024-05-11 19:37:46 +0100

    Numbered main menu options

--- week1/scr/app.py
+++ week1/scr/app.py
@@ -1,7 +1,8 @@
 product_names = ["coke-zero", "corona", "water", "sprite"]
 
-print("Exit App")
-print("Print Products List")
-print("Create New Produc")
-print("Update EXisting Product")
-print("Delete Product")
+print("0-Exit App")
+print("1-Print Products List")
+print("2-Create New Produc")
+print("3-Update EXisting Product")
+print("4-Delete Product")
+
