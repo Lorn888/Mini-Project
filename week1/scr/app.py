f126e40d2ccc121f1d0a03360f1a1db30a1e5f48
Patryk Okpara <patrykokpara@hotmail.com>
2024-05-12 18:42:54 +0100

    Main menu option 1

--- week1/scr/app.py
+++ week1/scr/app.py
@@ -1,15 +1,24 @@
-product_names = ["coke-zero", "corona", "water", "sprite"]
+product_list = ["coke-zero", "corona", "water", "sprite"]
 
 main_menu_options = ["0-Exit App", "1-Product Menu"]
 
-product_menu_options = ["0-Exit App","1-Products List","2-Create New Produc","3-Update EXisting Product"]
+product_menu_options = ["0-Return to the Main Menu","1-Products List","2-Create New Produc","3-Update EXisting Product"]
 
 print(main_menu_options)
 
-user_input= int(input("Please chose from the above 2 options '0-1'"))
+main_menu_input= int(input("Please chose from the above 2 options '0-1'"))
 
-if user_input == 0:
+if main_menu_input == 0:
     exit()
-elif user_input == 1:
+elif main_menu_input == 1:
     print(product_menu_options)
-    product_menu_inpout = input("Chose from above product menu")
+    product_menu_input = input("Chose from above product menu")
+    if product_menu_input == 0:
+        # Figure out how to return to the main menu
+        exit()
+    elif product_menu_input == 1:
+        print(product_list)
+
+
+
+        
