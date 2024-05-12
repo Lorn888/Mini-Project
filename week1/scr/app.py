9373d4ac83731e094e0697edc01163f0290619cb
Patryk Okpara <patrykokpara@hotmail.com>
2024-05-13 00:07:32 +0100

    Formatted

--- week1/scr/app.py
+++ week1/scr/app.py
@@ -2,14 +2,19 @@ product_list = ["coke-zero", "corona", "water", "sprite"]
 
 main_menu_options = ["0-Exit App", "1-Product Menu"]
 
-product_menu_options = ["0-Return to the Main Menu","1-Products List","2-Create New Produc","3-Update EXisting Product, 4-Delete Product"]
+product_menu_options = [
+    "0-Return to the Main Menu",
+    "1-Products List",
+    "2-Create New Produc",
+    "3-Update EXisting Product, 4-Delete Product",
+]
 
 print(main_menu_options)
 
 while True:
-    main_menu_input= int(input("Chose from the above 2 options '0-1'"))
+    main_menu_input = int(input("Chose from the above 2 options '0-1'"))
     if main_menu_input == 0:
-       break
+        break
     elif main_menu_input == 1:
         while True:
             print(product_menu_options)
@@ -26,19 +31,17 @@ while True:
                 product_list.append(new_product_input)
 
             elif product_menu_input == 3:
-               
+
                 for product in product_list:
                     print(f"{product_list.index(product)}-{product}")
                 product_to_update_input = int(input("Chose the product to update"))
                 new_product_name = input("Type the name of the new product")
                 product_list[product_to_update_input] = new_product_name
                 print(product_list)
-                
+
             elif product_menu_input == 4:
                 for product in product_list:
                     print(f"{product_list.index(product)}-{product}")
                 product_to_delete = int(input("Chose the product to delete"))
                 product_list.remove(product_list[product_to_delete])
                 print(product_list)
-
-        
