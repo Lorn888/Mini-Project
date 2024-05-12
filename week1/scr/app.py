d05449dd3cc157a44f98466e25c42bfada6aba1b
Patryk Okpara <patrykokpara@hotmail.com>
2024-05-12 10:32:40 +0100

    If user input is 1" - print menu options

--- week1/scr/app.py
+++ week1/scr/app.py
@@ -1,12 +1,11 @@
 product_names = ["coke-zero", "corona", "water", "sprite"]
 
-print("0-Exit App")
-print("1-Print Products List")
-print("2-Create New Produc")
-print("3-Update EXisting Product")
-print("4-Delete Product")
+
+product_menu_options = ["0-Exit App","1-Print Products List","2-Create New Produc","3-Update EXisting Product"]
 
 user_input= int(input("Please chose from the above options '0-4'"))
 
 if user_input == 0:
     exit()
+elif user_input == 1:
+    print(product_menu_options)
