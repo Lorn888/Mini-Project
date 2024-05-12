b590a1f18058b2c0e5916fc2166a5bebd7d3a5f0
Patryk Okpara <patrykokpara@hotmail.com>
2024-05-12 18:34:38 +0100

    Corrections

--- week1/scr/app.py
+++ week1/scr/app.py
@@ -1,11 +1,15 @@
 product_names = ["coke-zero", "corona", "water", "sprite"]
 
+main_menu_options = ["0-Exit App", "1-Product Menu"]
 
-product_menu_options = ["0-Exit App","1-Print Products List","2-Create New Produc","3-Update EXisting Product"]
+product_menu_options = ["0-Exit App","1-Products List","2-Create New Produc","3-Update EXisting Product"]
 
-user_input= int(input("Please chose from the above options '0-4'"))
+print(main_menu_options)
+
+user_input= int(input("Please chose from the above 2 options '0-1'"))
 
 if user_input == 0:
     exit()
 elif user_input == 1:
     print(product_menu_options)
+    product_menu_inpout = input("Chose from above product menu")
